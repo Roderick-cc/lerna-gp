132c86dbc94eb64572055c3f701cf9664137acfb
Shide Luo <luoshidenet@gmail.com>
2021-05-12 19:01:58 +0800

    feat:test

--- packages/daybyday-test/test.js
+++ packages/daybyday-test/test.js
@@ -2,11 +2,11 @@
  * @Description: 
  * @Author: shide
  * @Date: 2021-05-12 18:32:16
- * @LastEditTime: 2021-05-12 18:32:53
- * @FilePath: /lerna-gp/packages/daybyday/test.js
+ * @LastEditTime: 2021-05-12 18:57:56
+ * @FilePath: /xrkmm_taro_m/Users/shide/test/lerna-gp/packages/daybyday-test/test.js
  */
 
 
 function test() {
-
+123123
 }
