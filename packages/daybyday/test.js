cc89a8f1673835a1dc141d86b77937b86ce47f5b
Shide Luo <luoshidenet@gmail.com>
2021-05-12 18:51:37 +0800

    featLtest

---
+++ packages/daybyday/test.js
@@ -0,0 +1,12 @@
+/*
+ * @Description: 
+ * @Author: shide
+ * @Date: 2021-05-12 18:32:16
+ * @LastEditTime: 2021-05-12 18:32:53
+ * @FilePath: /lerna-gp/packages/daybyday/test.js
+ */
+
+
+function test() {
+
+}
